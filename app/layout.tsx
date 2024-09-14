8484117f750833bfce6249c5eca7d8155947f6c5
nawawishkid <nawawish.samerpark@gmail.com>
2024-09-14 12:00:08 +0700

    feat: add favicon files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/nav-bar/NavBar";
 import SectionCta from "@/components/section-cta/SectionCta";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter, Noto_Sans_Thai } from "next/font/google";
 import "./globals.css";
 
@@ -14,6 +14,20 @@ const notoSansThai = Noto_Sans_Thai({
 export const metadata: Metadata = {
   title: "Shiwawan Studio",
   description: "Web design and development studio",
+  icons: [
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png", sizes: "180x180" },
+    { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32" },
+    { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16" },
+    { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#c1ffce" },
+  ],
+  manifest: "/site.webmanifest",
+  other: {
+    "msapplication-TileColor": "#da532c",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
