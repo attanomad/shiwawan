f1c65d1c9458459a02f9adabeeaaa0dd5145c2fc
nawawishkid <nawawish.samerpark@gmail.com>
2024-10-14 09:18:26 +0700

    feat(i18n) :construction: (WIP) translated section cta

--- components/section-cta/SectionCta.tsx
+++ components/section-cta/SectionCta.tsx
@@ -1,17 +1,15 @@
+import { getTranslations } from "next-intl/server";
 import SectionTitle from "../section/SectionTitle";
 
-const content = {
-  title: "มีโปรเจกต์ในใจใช่ไหม?",
-  cta: "พูดคุยกับเรา",
-};
+export default async function SectionCta() {
+  const t = await getTranslations("SectionCta");
 
-export default function SectionCta() {
   return (
     <section className="flex flex-col px-4 md:px-12 lg:px-16 py-16 md:py-24 lg:py-32 gap-8 md:gap-12 lg:gap-16 bg-accent-six">
-      <SectionTitle>{content.title}</SectionTitle>
+      <SectionTitle>{t("title")}</SectionTitle>
       <div className="flex flex-col justify-center items-center gap-2 md:gap-4 xl:gap-[37px]">
         <button className="text-white px-4 md:px-6 py-2 md:py-4 font-bold text-base md:text-[1.75rem] xl:text-[1.75rem] bg-gradient-to-r from-accent-one via-accent-two to-accent-one rounded-[4rem]">
-          {content.cta}
+          {t("cta")}
         </button>
       </div>
     </section>
