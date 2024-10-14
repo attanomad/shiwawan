443d8612cd77c05b2019092e7b2dfbcb26ed3db9
nawawishkid <nawawish.samerpark@gmail.com>
2024-10-14 10:56:34 +0700

    fix async component

--- components/footer/LocaleSwitcher.tsx
+++ components/footer/LocaleSwitcher.tsx
@@ -2,7 +2,7 @@ import { routing } from "@/i18n/routing";
 import { useLocale, useTranslations } from "next-intl";
 import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
 
-export default async function LocaleSwitcher() {
+export default function LocaleSwitcher() {
   const t = useTranslations("LocaleSwitcher");
   const locale = useLocale();
 
