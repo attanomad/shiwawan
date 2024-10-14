e91a452054ffd3d1478b6b8336c0d29b655ea51a
nawawishkid <nawawish.samerpark@gmail.com>
2024-10-14 09:50:33 +0700

    fix(cta) :bug: add LINE url to CTA in the section

--- components/section-cta/SectionCta.tsx
+++ components/section-cta/SectionCta.tsx
@@ -1,5 +1,8 @@
+import { socialNetworkIndex } from "@/content/social-network";
 import { getTranslations } from "next-intl/server";
 import SectionTitle from "../section/SectionTitle";
+
+const lineSocialNetwork = socialNetworkIndex["line"];
 
 export default async function SectionCta() {
   const t = await getTranslations("SectionCta");
@@ -8,9 +11,13 @@ export default async function SectionCta() {
     <section className="flex flex-col px-4 md:px-12 lg:px-16 py-16 md:py-24 lg:py-32 gap-8 md:gap-12 lg:gap-16 bg-accent-six">
       <SectionTitle>{t("title")}</SectionTitle>
       <div className="flex flex-col justify-center items-center gap-2 md:gap-4 xl:gap-[37px]">
-        <button className="text-white px-4 md:px-6 py-2 md:py-4 font-bold text-base md:text-[1.75rem] xl:text-[1.75rem] bg-gradient-to-r from-accent-one via-accent-two to-accent-one rounded-[4rem]">
+        <a
+          href={lineSocialNetwork.url}
+          target="_blank"
+          className="text-white px-4 md:px-6 py-2 md:py-4 font-bold text-base md:text-[1.75rem] xl:text-[1.75rem] bg-gradient-to-r from-accent-one via-accent-two to-accent-one rounded-[4rem]"
+        >
           {t("cta")}
-        </button>
+        </a>
       </div>
     </section>
   );
